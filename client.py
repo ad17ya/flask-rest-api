42452779530419e26ebc18844d2eb97b50c3fde2
Aditya Kshirsagar <kaditya.anil@gmail.com>
2022-10-02 22:43:06 +0530

    added request types

--- client.py
+++ client.py
@@ -1,5 +1,19 @@
 from urllib import response
 import requests
 
-response = requests.get("http://127.0.0.1:5000/articles")
+response = requests.get("http://127.0.0.1:5000/hello?name=Aditya")
+print(response.text)
+
+response = requests.post("http://127.0.0.1:5000/echo")
+print(response.text)
+
+headers = {'Content-Type': 'application/json'}
+response = requests.post("http://127.0.0.1:5000/messages", headers=headers, data='{"message":"Aditya"}')
+
+# send a binary with requests
+
+data = open('./data/moon_sky.jpg', 'rb').read()
+
+headers = {'Content-Type': 'application/octet-stream'}
+response = requests.post("http://127.0.0.1:5000/messages", headers=headers, data=data)
 print(response.text)
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 # Implementing a REST API with Python and Flask
 
 from flask import Flask, url_for
+from flask import request
+from flask import json
 
 app = Flask(__name__)
 
@@ -15,6 +17,48 @@ def api_articles():
 @app.route('/articles/<articleid>')
 def api_article(articleid):
     return "You are reading " + articleid
+
+@app.route('/hello')
+def api_hello():
+    if 'name' in request.args:
+        return 'Hello ' + request.args['name']
+    else:
+        return 'Hello Anon'
+
+@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return 'GET'
+    elif request.method == 'POST':
+        return 'POST'
+    elif request.method == 'PATCH':
+        return 'PATCH'
+    elif request.method == 'PUT':
+        return 'PUT'
+    elif request.method == 'DELETE':
+        return 'DELETE'
+
+@app.route('/messages', methods=['POST'])
+def api_message():
+    try:
+        if request.headers['Content-Type'] == 'text/plain':
+            return "Text Message: " + request.data
+
+        elif request.headers['Content-Type'] == 'application/json':
+            return "JSON Message: " + json.dumps(request.json)
+        
+        elif request.headers['Content-Type'] == 'application/octet-stream':
+            print("here")
+            f = open('./write_binary', 'wb')
+            f.write(request.data)
+            f.close()
+            return "Binary message written!"
+
+        else:
+            return "415 Unsupported Media Type ;)"
+
+    except Exception as err:
+        print(err)
 
 if __name__ == '__main__':
     app.run()
